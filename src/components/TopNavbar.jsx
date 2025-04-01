41684ccf7fedaab127bd1dd1bf65c6dcaf43a232
KARKIamit001 <karkiamit036@gmail.com>
2025-03-31 19:09:36 -0700

    somgnfij

--- src/components/TopNavbar.jsx
+++ src/components/TopNavbar.jsx
@@ -7,36 +7,35 @@ import { NavLink } from "react-router";
 
 export default function TopNavbar() {
   return (
-   <div className="bg-gradient-to-r from-purple-700 via-blue-300 to-pink-500">
-     <div className="s:11/12 md:w-8/12 mx-auto  flex justify-between py-2 text-white">
-      <div className="">
-      <div className="flex gap-4 hidden md:block">
-        <p>
-          <Blinds />
-        </p>
-        <p>
-          <Instagram />
-        </p>
-        <p>
-          {" "}
-          <Twitter />
-        </p>
-        <p>
-          {" "}
-          <Send />
-        </p>
+    <div className="bg-gradient-to-r from-purple-700 via-blue-300 to-pink-500">
+      <div className="s:11/12 md:w-8/12 mx-auto  flex justify-between py-2 text-white">
+        <div className=" hidden md:block">
+          <div className="flex gap-4">
+            <p>
+              <Blinds />
+            </p>
+            <p>
+              <Instagram />
+            </p>
+            <p>
+              {" "}
+              <Twitter />
+            </p>
+            <p>
+              {" "}
+              <Send />
+            </p>
+          </div>
+        </div>
 
-      </div>
-      </div>
-
-      <div className="flex items-center gap-4 ">
-        <NavLink to="/about">About</NavLink>
-        <p>-</p>
-        <NavLink to="/login">Login</NavLink>
-        <p>-</p>
-        <NavLink to="/contact">Contact</NavLink>
+        <div className="flex items-center gap-4 ">
+          <NavLink to="/about">About</NavLink>
+          <p>-</p>
+          <NavLink to="/login">Login</NavLink>
+          <p>-</p>
+          <NavLink to="/contact">Contact</NavLink>
+        </div>
       </div>
     </div>
-   </div>
   );
 }
